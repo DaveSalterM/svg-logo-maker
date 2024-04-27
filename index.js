535b911e70d8ac4347795b944c7fad0b10b3a179
DaveSalterM <davesalter1988@gmail.com>
2024-04-27 14:19:30 -0700

    inquirer/shapes

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
+
+inquirer
+	.prompt([
+		{
+			type: 'input',
+			message: 'Enter up to 3 letters for logo: ',
+			name: '',
+		},
+		{
+			type: 'input',
+			message: 'Enter TEXT color (red, green, blue, etc.): ',
+			name: '',
+		},
+		{
+			type: 'list',
+			message: 'Choose a logo shape: ',
+			name: '',
+			choices: [],
+		},
+		{
+			type: 'input',
+			message: 'Enter SHAPE color (red, green, blue, etc.): ',
+			name: '',
+		},
+	]);
