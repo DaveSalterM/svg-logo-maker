4ec25c88c43610297b47142eb69e86f4c7891827
DaveSalterM <davesalter1988@gmail.com>
2024-04-27 15:28:25 -0700

    logo generated

--- index.js
+++ index.js
@@ -1,27 +1,49 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
+const Circle = require('./lib/circle.js');
+const Square = require('./lib/square.js');
+const Triangle = require('./lib/triangle.js');
 
 inquirer
 	.prompt([
 		{
 			type: 'input',
 			message: 'Enter up to 3 letters for logo: ',
-			name: '',
+			name: 'text',
 		},
 		{
 			type: 'input',
-			message: 'Enter TEXT color (red, green, blue, etc.): ',
-			name: '',
+			message: 'Enter TEXT color (name ie "blue" or Hex including "#"): ',
+			name: 'textColor',
 		},
 		{
 			type: 'list',
 			message: 'Choose a logo shape: ',
-			name: '',
-			choices: [],
+			name: 'shape',
+			choices: ['circle', 'square', 'triangle'],
 		},
 		{
 			type: 'input',
-			message: 'Enter SHAPE color (red, green, blue, etc.): ',
-			name: '',
+			message: 'Enter SHAPE color (name ie "blue" or Hex including "#"): ',
+			name: 'shapeColor',
 		},
-	]);
+	])
+	.then((data) => {
+		let shape;
+		if (data.shape === 'circle') {
+			shape = new Circle(data.text, data.textColor, data.shapeColor);
+			shape = shape.renderString();
+		}
+		if (data.shape === 'square') {
+			shape = new Square(data.text, data.textColor, data.shapeColor);
+			shape = shape.renderString();
+		}
+		if (data.shape === 'triangle') {
+			shape = new Triangle(data.text, data.textColor, data.shapeColor);
+			shape = shape.renderString();
+		}
+
+		fs.writeFile('./examples/logo.svg', shape, (err) => {
+			err ? console.error(err) : console.log('Generated logo.svg');
+		});
+	});
